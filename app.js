4273212e5ba8d37d00e802c6a54d6b979ecccd9a
pashtetikss <105599592+pashtetikss@users.noreply.github.com>
2022-05-30 15:55:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const item = document.querySelector(".item")
+const placeholders = document.querySelectorAll(".placeholder")
+
+item.addEventListener("dragstart", dragstart)
+item.addEventListener("dragend", dragend)
+
+for(const placeholder of placeholders) {
+    placeholder.addEventListener("dragover", dragover)
+    placeholder.addEventListener("dragenter", dragenter)
+    placeholder.addEventListener("dragleave", dragleave)
+    placeholder.addEventListener("drop", dragdrop)
+}
+
+function dragstart(event) {
+    event.target.classList.add("hold")
+    setTimeout(() => event.target.classList.add("hide"), 0)
+}
+function dragend() {
+    event.target.className = "item"
+    //event.target.classList.remove("hide")
+}
+
+function dragover(event) { 
+    event.preventDefault()
+    console.log("drag over")
+}
+function dragenter(event) {
+    event.target.classList.add("hovered")
+    console.log("drag enter")
+}
+function dragleave(event) {
+    event.target.classList.remove("hovered")
+    console.log("drag leave")
+}
+function dragdrop(event) {
+    event.target.classList.remove("hovered")
+    console.log("drag drop")
+    event.target.append(item)
+}
